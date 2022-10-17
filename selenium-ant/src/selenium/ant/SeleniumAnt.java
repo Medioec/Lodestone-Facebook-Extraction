7876f17b92f21a5477ce643d9eb20ebe0cd139ce
2102596sit <2102596@sit.singaporetech.edu.sg>
2022-10-18 01:01:35 +0800

    slight changes

--- selenium-ant/src/selenium/ant/SeleniumAnt.java
+++ selenium-ant/src/selenium/ant/SeleniumAnt.java
@@ -6,7 +6,11 @@ package selenium.ant;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
-
+import javax.swing.JPanel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 /**
  *
  * @author Alford
@@ -17,9 +21,27 @@ public class SeleniumAnt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alford\\Documents\\NetBeansProjects\\iOSDeviceDataExtractor\\chromedriver.exe");           
-        WebDriver driver = new ChromeDriver(); 
-        driver.get("http://www.google.com/");    
+               
+        System.setProperty("webdriver.chrome.driver","A:\\Users\\JJ\\Documents\\GitHub\\Lodestone\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        
+      
+        driver.get("https://www.facebook.com/dyi/?referrer=yfi_settings");
+        WebElement fb = driver.findElement(By.name("email"));
+        fb.sendKeys("destinyblazer@live.com");
+        WebElement ps = driver.findElement(By.name("pass"));
+        //input ur own password and username
+        ps.sendKeys("p@ssw0rd");
+        WebElement login = driver.findElement(By.name("login"));
+        login.click();
+        
+        try{
+        Thread.sleep(5000);
+        driver.findElement(By.cssSelector("[aria-label=Download]")).click();
+        }
+        catch(InterruptedException ie){
+        }
+         
     }
     
 }
