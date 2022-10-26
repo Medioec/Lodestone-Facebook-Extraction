24061f34d30630ddfb11a2b05ba562f7fc4c930d
ShaneLimJiXuan <33240428+ShaneLimJiXuan@users.noreply.github.com>
2022-10-26 18:28:57 +0800

    completed where youre logged in

---
+++ LodestoneIngest/src/org/lodestone/facebookingest/pojo/ActiveSessionsV2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.lodestone.facebookingest.pojo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Shane Lim
+ */
+public class ActiveSessionsV2 {
+        public List<activeSessions> active_sessions_v2;
+    public class activeSessions{
+        public long createdTimeStamp;
+        public String ip_address;
+        public String user_agent;
+        public String datr_cookie;
+        public String device;
+        public String location;
+        public String app;
+        public String session_type;
+    }
+}
