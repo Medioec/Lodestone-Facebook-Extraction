2f2d37420bb5e756301752cd0bd86fbaf24ddb5b
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2022-10-27 05:55:54 +0800

    Added more ingesting

---
+++ LodestoneIngest/src/org/lodestone/facebookingest/pojo/TopicsV2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.lodestone.facebookingest.pojo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alford
+ */
+public class TopicsV2 {
+    public List<String> topics_v2;
+}
