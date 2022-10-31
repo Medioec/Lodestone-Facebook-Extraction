316578bb1676d86c504543088dbf8e8d13d6a43d
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2022-11-01 02:36:36 +0800

    Finally finished profile information processing

--- LodestoneIngest/src/org/lodestone/facebookingest/pojo/ProfileV2.java
+++ LodestoneIngest/src/org/lodestone/facebookingest/pojo/ProfileV2.java
@@ -15,18 +15,23 @@ public class ProfileV2 {
     public class Profile_V2 {
         public Name name;
         public Emails emails;
+        public Birthday birthday;
         public Gender gender;
         public List<PreviousNames> previous_names;
         public List<OtherNames> other_names;
         public Location current_city;
         public Location hometown;
         public Relationship relationship;
+        public List<PreviousRelationship> previous_relationships;
         public List<FamilyMembers> family_members;
         public List<EducationExperience> education_experiences;
         public List<WorkExperience> work_experiences;
         public List<Language> languages;
         public ReligiousView religious_view;
         public BloodInfo blood_info;
+        public List<Website> websites;
+        public List<ScreenName> screen_names;
+        public Address address;
         public List<PhoneNumber> phone_numbers;
         public String username;
         public String about_me;
@@ -46,7 +51,12 @@ public class ProfileV2 {
             public List<String> pending_emails;
             public List<String> ad_account_emails;
         }
-        public class Gender{
+        public class Birthday {
+            public String year;
+            public String month;
+            public String day;
+        }
+        public class Gender {
             public String gender_option;
             public String pronoun;
         }
@@ -65,6 +75,11 @@ public class ProfileV2 {
         }
         public class Relationship {
             public String status;
+            public String partner;
+            public long timestamp;
+        }
+        public class PreviousRelationship {
+            public String name;
             public long timestamp;
         }
         public class FamilyMembers {
@@ -76,11 +91,25 @@ public class ProfileV2 {
             public String name;
             public String graduated;
             public String school_type;
+            public String description;
+            public long start_timestamp;
+            public long end_timestamp;
             public long timestamp;
+            public List<String> concentrations;
+            public List<Module> modules;
+            public class Module {
+                public String name;
+                public List<Tag> tags;
+                public class Tag {
+                    public String name;
+                }
+            }
         }
         public class WorkExperience {
             public String employer;
             public String title;
+            public String location;
+            public String description;
             public long start_timestamp;
             public long end_timestamp;
             public long timestamp;
@@ -94,6 +123,24 @@ public class ProfileV2 {
         }
         public class BloodInfo {
             public String blood_donor_status;
+        }
+        public class Website {
+            public String address;
+        }
+        public class ScreenName {
+            public String service_name;
+            public List<Screen_Name> names;
+            public class Screen_Name {
+                public String name;
+                public long timestamp;
+            }
+        }
+        public class Address {
+            public String street;
+            public String city;
+            public String country;
+            public String country_code;
+            public String region;
         }
         public class PhoneNumber {
             public String phone_type;
