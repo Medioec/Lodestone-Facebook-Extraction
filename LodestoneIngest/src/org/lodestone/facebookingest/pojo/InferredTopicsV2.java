b9d27b412a91b1c749b5ddec99028a51e4bbb12b
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2022-11-01 18:40:19 +0800

    Finished last two processing JSON.

---
+++ LodestoneIngest/src/org/lodestone/facebookingest/pojo/InferredTopicsV2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.lodestone.facebookingest.pojo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Alford
+ */
+public class InferredTopicsV2 {
+    public List<String> inferred_topics_v2;
+}
